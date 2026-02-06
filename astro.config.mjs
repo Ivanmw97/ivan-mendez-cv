ec399182aab42c0884688135a80395989c24ca1a
Ivan Mendez <ivanmw97@gmail.com>
2026-02-06 00:01:50 +0000

    CV-022: add sitemap and robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ivanmendez.dev',
   output: 'static',
   adapter: vercel({}),
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
